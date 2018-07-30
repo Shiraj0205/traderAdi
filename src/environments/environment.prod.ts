7e1183a42831dc81c249df3bce5157c6c24f5100
sshaikh1 <shiraj.shaikh@gmail.com>
2018-07-29 20:05:46 -0500

    firebase configuration added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase :  {
+    apiKey: "AIzaSyAu0L8N4BlcSprarFaHROb6b9Ai8E4KcNA",
+    authDomain: "traderadi-7864f.firebaseapp.com",
+    databaseURL: "https://traderadi-7864f.firebaseio.com",
+    projectId: "traderadi-7864f",
+    storageBucket: "traderadi-7864f.appspot.com",
+    messagingSenderId: "948110998036"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :  {
+                apiKey: "AIzaSyAu0L8N4BlcSprarFaHROb6b9Ai8E4KcNA",
+                authDomain: "traderadi-7864f.firebaseapp.com",
+                databaseURL: "https://traderadi-7864f.firebaseio.com",
+                projectId: "traderadi-7864f",
+                storageBucket: "traderadi-7864f.appspot.com",
+                messagingSenderId: "948110998036"
+              }
 };
 
 /*
